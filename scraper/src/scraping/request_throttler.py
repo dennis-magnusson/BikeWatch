45e5ea5bf4d9754f5fbdb8f192af7b8c772c0257
Dennis Magnusson <magnden@gmail.com>
2024-12-18 12:03:43 +0200

    init new scraper

---
+++ scraper/src/scraping/request_throttler.py
@@ -0,0 +1,40 @@
+import time
+from functools import wraps
+import requests
+import logging
+
+
+def rate_limited_request(rate_limit_seconds):
+    """
+    A decorator to limit the rate of requests.
+
+    Args:
+        rate_limit_seconds (float): The minimum time (in seconds) between requests.
+    """
+    last_request_time = [0]
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            current_time = time.time()
+            elapsed_time = current_time - last_request_time[0]
+
+            if elapsed_time < rate_limit_seconds:
+                time_to_sleep = rate_limit_seconds - elapsed_time
+                logging.debug(
+                    f"Rate limit reached. Sleeping for {time_to_sleep:.2f} seconds."
+                )
+                time.sleep(time_to_sleep)
+
+            last_request_time[0] = time.time()
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@rate_limited_request(rate_limit_seconds=1.0)
+def get_request(url, **kwargs):
+    """Perform a GET request with rate limiting."""
+    return requests.get(url, **kwargs)
---
+++ scraper/src/scraping/scraper.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+
+from data.models import BikeListingData
+from scraping.parsing import (
+    parse_date_posted,
+    parse_raw_description,
+    parse_raw_images,
+    parse_raw_title,
+)
+from scraping.request_throttler import get_request
+
+
+def find_listings_for_category(base_url, pages=1):
+    urls = [base_url.format(page) for page in range(1, pages + 1)]
+
+    found_listings = []
+
+    for url in urls:
+        print(url)
+        response = get_request(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        ol = soup.find("ol", class_="ipsDataList")
+        listings = ol.find_all("h4")
+
+        found_listings.extend(
+            [
+                listing.find_all("a")[1].get("href")
+                for listing in listings
+                if len(listing.find_all("a")) >= 2
+            ]
+        )
+
+    return found_listings
+
+
+def get_listing_id(url: str) -> str:
+    return url.rstrip("/").split("/")[-1]
+
+
+def scrape_listing(url: str) -> BikeListingData:
+    response = get_request(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    id = get_listing_id(url)
+    title = parse_raw_title(soup)
+    date_posted = parse_date_posted(soup)
+    brand, model, price, year, size, region, city, description, short_description = (
+        parse_raw_description(soup)
+    )
+    images = parse_raw_images(soup)
+
+    return BikeListingData(
+        id=id,
+        title=title,
+        brand=brand,
+        model=model,
+        year=year,
+        url=url,
+        date_posted=date_posted,
+        size=size,
+        images=images,
+        price=price,
+        city=city,
+        region=region,
+        description=description,
+        short_description=short_description,
+    )
