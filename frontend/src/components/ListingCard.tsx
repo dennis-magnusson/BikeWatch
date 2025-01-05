70000b355ca0d246190faccacfe775c4b88fc214
Dennis Magnusson <magnden@gmail.com>
2025-01-05 11:54:34 +0200

    improve layout

--- frontend/src/components/ListingCard.tsx
+++ frontend/src/components/ListingCard.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { CardFooter } from "@/components/ui/card";
-import { ChevronLeft, ChevronRight, ExternalLink } from "lucide-react"; // Import carousel icons
+import { ChevronLeft, ChevronRight, ExternalLink, MapPin } from "lucide-react"; // Import carousel icons
 
 type ListingCardProps = {
     listing: Listing;
@@ -28,9 +28,9 @@ function ListingCard({ listing }: ListingCardProps) {
     };
 
     return (
-        <Card className="max-w-[330px]">
+        <Card className="w-[300px] mx-auto overflow-hidden flex flex-col">
             <div className="space-y-2">
-                <div className="relative w-full h-48 overflow-hidden">
+                <div className="relative w-full h-44">
                     <img
                         src={listing.images[selectedImage]}
                         alt={`Bike image ${selectedImage + 1}`}
@@ -41,23 +41,23 @@ function ListingCard({ listing }: ListingCardProps) {
                     </Badge>
                     <button
                         onClick={handlePrevImage}
-                        className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white p-1 rounded-full opacity-40"
+                        className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white p-1 rounded-full opacity-40 hover:opacity-80 transition-opacity"
                     >
-                        <ChevronLeft />
+                        <ChevronLeft className="h-6 w-6" />
                     </button>
                     <button
                         onClick={handleNextImage}
-                        className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white p-1 rounded-full opacity-40"
+                        className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white p-1 rounded-full opacity-40 hover:opacity-80 transition-opacity"
                     >
-                        <ChevronRight />
+                        <ChevronRight className="h-6 w-6" />
                     </button>
                 </div>
-                <div className="flex justify-center space-x-2 p-2 overflow-x-auto">
+                <div className="flex justify-center space-x-2 mt-1 px-1 py-1 overflow-x-auto">
                     {listing.images.map((image, index) => (
                         <button
                             key={index}
                             onClick={() => setSelectedImage(index)}
-                            className={`w-16 h-12 relative overflow-hidden rounded ${
+                            className={`w-16 h-12 relative overflow-hidden rounded border-none ${
                                 index === selectedImage
                                     ? "ring-2 ring-primary"
                                     : ""
@@ -66,30 +66,34 @@ function ListingCard({ listing }: ListingCardProps) {
                             <img
                                 src={image}
                                 alt={`Thumbnail ${index + 1}`}
-                                className="object-cover w-full h-full"
+                                className="object-cover absolute inset-0 w-full h-full"
                             />
                         </button>
                     ))}
                 </div>
             </div>
-            <CardHeader>
+            <CardHeader className="py-3">
                 <CardTitle className="text-xl">{listing.title}</CardTitle>
-                <p className="text-sm text-muted-foreground">
-                    {listing.city}, {listing.region}
+                <p className="text-sm text-muted-foreground flex items-center">
+                    <MapPin className="h-4 w-4 mr-1" /> {listing.city},{" "}
+                    {listing.region}
                 </p>
             </CardHeader>
-            <CardContent>
-                <p className="text-sm">{listing.size}</p>
+            <CardContent className="pb-3">
+                <Badge className="text-md" variant="outline">
+                    Size: {listing.size}
+                </Badge>
             </CardContent>
-            <CardFooter>
-                <Button
-                    variant="outline"
-                    className="w-full"
-                    as="a"
-                    href={listing.url}
-                    target="_blank"
-                >
-                    View on fillaritori.com <ExternalLink />
+            <CardFooter className="mt-auto pt-0">
+                <Button variant="outline" className="w-full" asChild>
+                    <a
+                        href={listing.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        View on fillaritori.com{" "}
+                        <ExternalLink className="ml-2 h-4 w-4" />
+                    </a>
                 </Button>
             </CardFooter>
         </Card>
