114189df7ddccccf4613e2a4f9e38291f0f6d169
Dennis Magnusson <magnden@gmail.com>
2025-01-10 16:41:29 +0200

    wip: size parsing not implemented yet

--- scraper/src/scraping/parse_size.py
+++ scraper/src/scraping/parse_size.py
@@ -14,4 +14,5 @@ def parse_size(
     - Returns the ranges (min and max) for letter sizes if present (None otherwise).
     - If the size is not a range the same value is returned for both min and max.
     """
+    raise NotImplementedError("Not implemented yet")
     return ("M", "L", 50.0, 52.0)
