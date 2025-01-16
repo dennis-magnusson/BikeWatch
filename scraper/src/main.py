d923c57c8688ad83183debdffdf7d00b70239fa5
Dennis Magnusson <magnden@gmail.com>
2025-01-16 19:10:34 +0200

    refactor main into a class

--- scraper/src/main.py
+++ scraper/src/main.py
@@ -1,11 +1,10 @@
 import logging
 import os
-from dataclasses import dataclass
 from time import sleep
 from typing import Iterable
 
-import scraping.urls
 from scraping.scraper import find_listings_for_category, scrape_listing
+from scraping.urls import categories, get_url
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from utils.db_operations import sync_listings
@@ -13,13 +12,62 @@ from utils.db_operations import sync_listings
 from common.models import Base, BikeListingData
 
 
+class Scraper:
+    def __init__(
+        self,
+        session: Session,
+        scraping_frequency_minutes: int,
+        scraping_page_limit: int,
+    ):
+        self.session = session
+        self.scraping_frequency_minutes = scraping_frequency_minutes
+        self.scraping_page_limit = scraping_page_limit
+
+    def scrape_category(self, category):
+        base_url = get_url(category)
+        category_name = category.name
+        logging.info(f"Scraping category ===== {category_name} =====")
+
+        listing_urls: str = find_listings_for_category(
+            base_url, pages=self.scraping_page_limit
+        )
+        logging.debug("Found {} listings".format(len(listing_urls)))
+
+        listings: Iterable[BikeListingData] = [
+            scrape_listing(listing, category_name=category.name)
+            for listing in listing_urls
+        ]
+        logging.debug("Scraped {} listings".format(len(listings)))
+
+        sync_listings(self.session, listings)
+        logging.info("Listings synced to database")
+
+    def run(self):
+        try:
+            while True:
+                logging.info("Scraping cycle started")
+                for category in categories:
+                    self.scrape_category(category)
+                logging.info(
+                    "Scraping cycle completed, sleeping for {} minutes".format(
+                        self.scraping_frequency_minutes
+                    )
+                )
+                sleep(self.scraping_frequency_minutes * 60)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+        finally:
+            logging.info("Closing sqlalchemy session...")
+            self.session.close()
+
+
 def main():
     logging.basicConfig(
         level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
     )
 
-    SCRAPING_FREQUENCY_MINUTES = int(os.environ.get("SCRAPING_FREQUENCY_MINUTES", 60))
-    SCRAPING_PAGE_LIMIT = int(os.environ.get("PAGE_COUNT", 1))
+    scraping_frequency_minutes = int(os.environ.get("SCRAPING_FREQUENCY_MINUTES", 60))
+    scraping_page_limit = int(os.environ.get("PAGE_COUNT", 1))
 
     logging.info("Opening sqlalchemy session to sqlite...")
     engine = create_engine("sqlite:///data/bikes.db")
@@ -33,40 +81,8 @@ def main():
     Base.metadata.create_all(engine)
     logging.info("Database schema created")
 
-    try:
-        while True:
-            logging.info("Scraping cycle started")
-
-            for category in scraping.urls.categories:
-                base_url = scraping.urls.get_url(category)
-                category_name = category.name
-                logging.info(f"Scraping category ===== {category_name} =====")
-
-                listing_urls: str = find_listings_for_category(
-                    base_url, pages=SCRAPING_PAGE_LIMIT
-                )
-                logging.debug("Found {} listings".format(len(listing_urls)))
-
-                listings: Iterable[BikeListingData] = [
-                    scrape_listing(listing, category_name=category.name)
-                    for listing in listing_urls
-                ]
-                logging.debug("Scraped {} listings".format(len(listings)))
-
-                sync_listings(session, listings)
-                logging.info("Listings synced to database")
-
-                logging.info(
-                    "Scraping cycle completed, sleeping for {} minutes".format(
-                        SCRAPING_FREQUENCY_MINUTES
-                    )
-                )
-            sleep(SCRAPING_FREQUENCY_MINUTES * 60)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-    finally:
-        logging.info("Closing sqlalchemy session...")
-        session.close()
+    scraper = Scraper(session, scraping_frequency_minutes, scraping_page_limit)
+    scraper.run()
 
 
 if __name__ == "__main__":
