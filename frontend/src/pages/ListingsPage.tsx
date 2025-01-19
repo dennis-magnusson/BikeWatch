fbc25c12542386ae95300350a062e8c992837f9d
Dennis Magnusson <magnden@gmail.com>
2025-01-19 22:27:09 +0200

    use env variable to give frontend api base url

--- frontend/src/pages/ListingsPage.tsx
+++ frontend/src/pages/ListingsPage.tsx
@@ -27,7 +27,7 @@ function ListingsPage() {
     const resultsPerPage = 30;
     const totalPages = Math.ceil(totalResults / resultsPerPage);
 
-    const BASE_URL = "http://127.0.0.1:8000";
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
     useEffect(() => {
         fetchListings();
