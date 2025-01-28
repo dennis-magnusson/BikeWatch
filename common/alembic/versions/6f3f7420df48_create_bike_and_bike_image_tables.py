f3a85894a3c7e3c971f6796913a59331ad246945
Dennis Magnusson <magnden@gmail.com>
2025-01-28 11:30:30 +0200

    get rid of redundant migrations, make consistent with orm models

--- common/alembic/versions/6f3f7420df48_create_bike_and_bike_image_tables.py
+++ common/alembic/versions/6f3f7420df48_create_bike_and_bike_image_tables.py
@@ -27,20 +27,26 @@ def upgrade() -> None:
         sa.Column("model", sa.String),
         sa.Column("year", sa.Integer),
         sa.Column("url", sa.String, nullable=False),
-        sa.Column("date_posted", sa.DateTime),
-        sa.Column("size", sa.String),
+        sa.Column("date_posted", sa.DateTime, nullable=True),
+        sa.Column("number_size_min", sa.Float, nullable=True),
+        sa.Column("number_size_max", sa.Float, nullable=True),
+        sa.Column("letter_size_min", sa.String, nullable=True),
+        sa.Column("letter_size_max", sa.String, nullable=True),
         sa.Column("price", sa.Float),
         sa.Column("city", sa.String),
         sa.Column("region", sa.String),
         sa.Column("description", sa.String),
         sa.Column("short_description", sa.String),
+        sa.Column("category", sa.String),
+        sa.Column("date_first_seen", sa.DateTime, server_default=sa.func.now()),
+        sa.Column("date_last_updated", sa.DateTime, server_default=sa.func.now()),
     )
 
     op.create_table(
         "bike_image",
         sa.Column("id", sa.Integer, primary_key=True),
         sa.Column("bike_id", sa.Integer, sa.ForeignKey("bike.id")),
-        sa.Column("url", sa.String, nullable=False),
+        sa.Column("image_url", sa.String, nullable=False),
     )
 
 
--- common/alembic/versions/9b1716b0ce95_create_tables_for_alerts.py
+++ common/alembic/versions/9b1716b0ce95_create_tables_for_alerts.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9b1716b0ce95"
-down_revision: Union[str, None] = "0294a402e296"
+down_revision: Union[str, None] = "6f3f7420df48"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
