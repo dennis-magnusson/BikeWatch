8d5b4527c3e73997e3094c1f0dbc86714133d1a8
Dennis Magnusson <magnden@gmail.com>
2025-02-13 12:11:04 +0200

    backend: WIP refactor telegram bot

--- backend/src/telegram_bot/handlers.py
+++ backend/src/telegram_bot/handlers.py
@@ -1,172 +1,76 @@
-from sqlalchemy.orm import Session
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    Update,
+)
 from telegram.ext import ContextTypes
 
-from backend.src.database import get_db
-from common.models.alert import UserAlert
+BIKE_CATEGORY, BIKE_MAXPRICE, BIKE_SIZE = range(3)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    args = context.args
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    reply_keyboard = [
+        ["Road", "Gravel", "Hybrid", "Triathlon", "Fatbike", "Electric"],
+    ]
 
-    if len(args) != 0:  # if chat_id argument is given
-        alert_id = args[0]
-        chat_id = update.effective_chat.id
+    await update.message.reply_text(
+        "<b>Welcome to the BikeWatch Bot!\n"
+        "Let's get some details about the bikes you want me to watch for\n"
+        "What type of bike are you looking for?</b>",
+        parse_mode="HTML",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+        ),
+    )
 
-        db: Session = next(get_db())
-
-        user_alert = db.query(UserAlert).filter(UserAlert.id == alert_id).first()
-
-        if user_alert:
-            user_alert.chat_id = chat_id
-            db.commit()
-            await context.bot.send_message(
-                chat_id=chat_id,
-                text="Your alert has been successfully registered.",
-            )
-        else:
-            await context.bot.send_message(
-                chat_id=chat_id,
-                text="The id you provided doesn't exist or then it has already been registered.",
-            )
-    else:
-        keyboard = [
-            [
-                InlineKeyboardButton("Yes", callback_data="create_filter_yes"),
-                InlineKeyboardButton("No", callback_data="create_filter_no"),
-            ]
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await update.message.reply_text(
-            "Do you want to create a new filter?",
-            reply_markup=reply_markup,
-        )
+    return BIKE_CATEGORY
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Use /start to test this bot.")
+async def bike_category(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["bike_category"] = update.message.text
+    await update.message.reply_text(
+        f"<b>You selected {update.message.text}.\n" f"What is your budget?</b>",
+        parse_mode="HTML",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    keyboard = [
+        [InlineKeyboardButton("Any price", callback_data="maxprice_any")],
+        [InlineKeyboardButton("100€", callback_data="maxprice_100")],
+        [InlineKeyboardButton("500€", callback_data="maxprice_500")],
+        [InlineKeyboardButton("1000€", callback_data="maxprice_1000")],
+        [InlineKeyboardButton("2000€", callback_data="maxprice_2000")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "<b>Please choose:</b>", parse_mode="HTML", reply_markup=reply_markup
+    )
+
+    return BIKE_MAXPRICE
 
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def bike_maxprice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
+    query.answer()
 
-    if query.data == "create_filter_yes":
-        await query.answer()
-        await query.edit_message_text(
-            text="What frame size are you looking for? Give the tube length in centimeters (eg. 54 if you are looking for a 54cm frame)."
-        )
-        context.user_data["filter_stage"] = "size"
-        return
-    elif query.data == "create_filter_no":
-        await query.answer()
-        await query.edit_message_text(
-            text="You can create a new filter later. Just use the command /start."
-        )
-        return
+    context.user_data["bike_maxprice"] = query.data
+    await query.message.reply_text(
+        f"<b>You selected {query.data}.\n" f"What size are you looking for?</b>",
+        parse_mode="HTML",
+    )
 
-    # Handle size filter response
-    if query.data.startswith("size_filter_"):
-        await query.answer()
-        keyboard = [
-            [InlineKeyboardButton("Road", callback_data="category_filter_road")],
-            [InlineKeyboardButton("Gravel", callback_data="category_filter_gravel")],
-            [InlineKeyboardButton("Hybrid", callback_data="category_filter_hybrid")],
-            [
-                InlineKeyboardButton(
-                    "Triathlon", callback_data="category_filter_triathlon"
-                )
-            ],
-            [InlineKeyboardButton("Fatbike", callback_data="category_filter_fatbike")],
-            [
-                InlineKeyboardButton(
-                    "Electric", callback_data="category_filter_electric_flat"
-                )
-            ],
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_text(
-            text="What category of bike?", reply_markup=reply_markup
-        )
-        return
-
-    # Handle category filter response
-    if query.data.startswith("category_filter_"):
-        await query.answer()
-        keyboard = [
-            [InlineKeyboardButton("Any", callback_data="price_range_any")],
-            [InlineKeyboardButton("0€ - 500€", callback_data="price_range_0_500")],
-            [
-                InlineKeyboardButton(
-                    "500€ - 1000€", callback_data="price_range_500_1000"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "1000€ - 2000€", callback_data="price_range_1000_2000"
-                )
-            ],
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_text(
-            text="What price range are you looking for?", reply_markup=reply_markup
-        )
-        return
+    return BIKE_SIZE
 
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if "filter_stage" in context.user_data:
-        if context.user_data["filter_stage"] == "size":
-            size = update.message.text
-            try:
-                size = int(size)
-                if 42 <= size <= 68:
-                    context.user_data["size"] = size
-                    await update.message.reply_text(
-                        f"Size {size}cm added. Now, please choose bike category."
-                    )
-                    context.user_data["filter_stage"] = "category"
-                    keyboard = [
-                        [
-                            InlineKeyboardButton(
-                                "Road", callback_data="category_filter_road"
-                            )
-                        ],
-                        [
-                            InlineKeyboardButton(
-                                "Gravel", callback_data="category_filter_gravel"
-                            )
-                        ],
-                        [
-                            InlineKeyboardButton(
-                                "Hybrid", callback_data="category_filter_hybrid"
-                            )
-                        ],
-                        [
-                            InlineKeyboardButton(
-                                "Triathlon", callback_data="category_filter_triathlon"
-                            )
-                        ],
-                        [
-                            InlineKeyboardButton(
-                                "Fatbike", callback_data="category_filter_fatbike"
-                            )
-                        ],
-                        [
-                            InlineKeyboardButton(
-                                "Electric",
-                                callback_data="category_filter_electric_flat",
-                            )
-                        ],
-                    ]
-                    reply_markup = InlineKeyboardMarkup(keyboard)
-                    await update.message.reply_text(
-                        text="Please choose categories to filter.",
-                        reply_markup=reply_markup,
-                    )
-                else:
-                    await update.message.reply_text(
-                        "Invalid size. Please enter a number between 42 and 68."
-                    )
-            except ValueError:
-                await update.message.reply_text("Invalid size. Please enter a number.")
-        return
+async def bike_size(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["bike_size"] = update.message.text
+    await update.message.reply_text(
+        f"<b>You selected {update.message.text}.\n"
+        f"Great! I will start looking for bikes that match your criteria.</b>",
+        parse_mode="HTML",
+    )
+
+    return BIKE_SIZE
