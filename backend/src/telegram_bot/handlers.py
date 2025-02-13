7508fc3618d9cf2f222ad2918642002c05b50235
Dennis Magnusson <magnden@gmail.com>
2025-02-13 15:00:27 +0200

    backend: WIP refactor telegram bot

--- backend/src/telegram_bot/handlers.py
+++ backend/src/telegram_bot/handlers.py
@@ -1,24 +1,29 @@
 from telegram import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
     ReplyKeyboardMarkup,
     ReplyKeyboardRemove,
     Update,
 )
-from telegram.ext import ContextTypes
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
-BIKE_CATEGORY, BIKE_MAXPRICE, BIKE_SIZE = range(3)
+BIKE_CATEGORY, BIKE_MAXPRICE, BIKE_MINPRICE, BIKE_SIZE = range(4)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_keyboard = [
-        ["Road", "Gravel", "Hybrid", "Triathlon", "Fatbike", "Electric"],
+        ["Road", "Gravel", "Hybrid", "All"],
     ]
 
     await update.message.reply_text(
-        "<b>Welcome to the BikeWatch Bot!\n"
-        "Let's get some details about the bikes you want me to watch for\n"
-        "What type of bike are you looking for?</b>",
+        "Welcome to the BikeWatch Bot!\n"
+        "Let's get some details about the bikes you want me to watch for.\n\n"
+        "What type of bike are you looking for?",
         parse_mode="HTML",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, resize_keyboard=True
@@ -31,46 +36,85 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 async def bike_category(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data["bike_category"] = update.message.text
     await update.message.reply_text(
-        f"<b>You selected {update.message.text}.\n" f"What is your budget?</b>",
-        parse_mode="HTML",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    keyboard = [
-        [InlineKeyboardButton("Any price", callback_data="maxprice_any")],
-        [InlineKeyboardButton("100€", callback_data="maxprice_100")],
-        [InlineKeyboardButton("500€", callback_data="maxprice_500")],
-        [InlineKeyboardButton("1000€", callback_data="maxprice_1000")],
-        [InlineKeyboardButton("2000€", callback_data="maxprice_2000")],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.message.reply_text(
-        "<b>Please choose:</b>", parse_mode="HTML", reply_markup=reply_markup
+        "What is your budget in euros?",
     )
 
     return BIKE_MAXPRICE
 
 
 async def bike_maxprice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    query = update.callback_query
-    query.answer()
+    try:
+        max_price = float(update.message.text)
+        if max_price >= 0:
+            context.user_data["bike_maxprice"] = max_price
+            await update.message.reply_text(
+                "What is the minimum value in euros of bikes you are interested in?",
+            )
+            return BIKE_MINPRICE
+        else:
+            await update.message.reply_text(
+                "Invalid price. Please enter a non-negative number."
+            )
+    except ValueError:
+        await update.message.reply_text("Invalid price. Please enter a valid number.")
+    return BIKE_MAXPRICE
 
-    context.user_data["bike_maxprice"] = query.data
-    await query.message.reply_text(
-        f"<b>You selected {query.data}.\n" f"What size are you looking for?</b>",
-        parse_mode="HTML",
-    )
 
-    return BIKE_SIZE
+async def bike_minprice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    try:
+        # TODO: Handle "inf"
+        min_price = float(update.message.text)
+        if min_price >= 0 and min_price < context.user_data["bike_maxprice"]:
+            context.user_data["bike_minprice"] = min_price
+            await update.message.reply_text(
+                "What size are you looking for? Please provide the tube length in centimeters.",
+            )
+            return BIKE_SIZE
+        else:
+            await update.message.reply_text(
+                "Invalid price. Please enter a non-negative number that less than your max price."
+            )
+    except ValueError:
+        await update.message.reply_text("Invalid price. Please enter a valid number.")
+    return BIKE_MINPRICE
 
 
 async def bike_size(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    context.user_data["bike_size"] = update.message.text
-    await update.message.reply_text(
-        f"<b>You selected {update.message.text}.\n"
-        f"Great! I will start looking for bikes that match your criteria.</b>",
-        parse_mode="HTML",
-    )
+    try:
+        size = float(update.message.text)
+        if size > 0:
+            context.user_data["bike_size"] = update.message.text
+            await update.message.reply_text(
+                "Great! I will send you bikes that match these criteria.\n\n"
+                f"Category: {context.user_data['bike_category']}\n"
+                f"Price range: {context.user_data['bike_minprice']}-{context.user_data['bike_maxprice']} €\n"
+                f"Size: {context.user_data['bike_size']} cm"
+            )
 
-    return BIKE_SIZE
+            # TODO: register a new alert to the db: update.effective_user.id, bike_category, bike_maxprice, bike_size
+        else:
+            await update.message.reply_text(
+                "Invalid size. Please enter a positive number."
+            )
+    except ValueError:
+        await update.message.reply_text("Invalid size. Please enter a valid number.")
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data.clear()
+    await update.message.reply_text("Bye!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+conversation_handler = ConversationHandler(
+    entry_points=[CommandHandler("start", start)],
+    states={
+        BIKE_CATEGORY: [MessageHandler(filters.TEXT & ~filters.COMMAND, bike_category)],
+        BIKE_MAXPRICE: [MessageHandler(filters.TEXT & ~filters.COMMAND, bike_maxprice)],
+        BIKE_MINPRICE: [MessageHandler(filters.TEXT & ~filters.COMMAND, bike_minprice)],
+        BIKE_SIZE: [MessageHandler(filters.TEXT & ~filters.COMMAND, bike_size)],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
--- backend/src/telegram_bot/telegram_bot.py
+++ backend/src/telegram_bot/telegram_bot.py
@@ -2,41 +2,21 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram.ext import (
-    ApplicationBuilder,
-    CallbackQueryHandler,
-    CommandHandler,
-    MessageHandler,
-    filters,
-)
+from telegram import Update
+from telegram.ext import ApplicationBuilder
 
-from backend.src.telegram_bot.handlers import (
-    button,
-    handle_message,
-    help_command,
-    start,
-)
+from backend.src.telegram_bot.handlers import conversation_handler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 load_dotenv()
-
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
 if not TELEGRAM_BOT_TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN is not set in .env file")
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
-
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
-    application.add_handler(CallbackQueryHandler(button))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(
-        MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
-    )
-
-    application.run_polling()
+    application.add_handler(conversation_handler)
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
