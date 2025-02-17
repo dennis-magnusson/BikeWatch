42c6efa773f11ccf758c2922a96e003db0d945fe
Dennis Magnusson <magnden@gmail.com>
2025-02-17 14:01:47 +0200

    move database module from backend to common

--- backend/src/routes/alerts.py
+++ backend/src/routes/alerts.py
@@ -2,9 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from common import UserAlert
+from common.database import get_db
 from common.schemas.user_alert import UserAlertCreate, UserAlertResponse
-
-from ..database import get_db
 
 router = APIRouter()
 
